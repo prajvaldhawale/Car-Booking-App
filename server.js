165144aee7275d855c2433c27e49b81a7d310ffd
Prajval Dhawale <132567838+prajvaldhawale@users.noreply.github.com>
2025-10-06 18:20:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/', (req, res) => {
-  res.send('Car Booking API is running on the 🚗.....');
+  res.send('Car Booking API is running on the jenkins 🚗.....');
 });
 
